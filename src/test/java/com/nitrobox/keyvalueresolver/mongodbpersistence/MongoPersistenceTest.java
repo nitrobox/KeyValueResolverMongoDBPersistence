648cba9abb93eb8165a071551821c0bde972a628
HeikoNitrobox <99733931+HeikoNitrobox@users.noreply.github.com>
2022-11-24 09:04:51 +0100

    NBX-8455 add license and upgrade libs

--- src/test/java/com/nitrobox/keyvalueresolver/mongodbpersistence/MongoPersistenceTest.java
+++ src/test/java/com/nitrobox/keyvalueresolver/mongodbpersistence/MongoPersistenceTest.java
@@ -1,3 +1,21 @@
+/*
+ * KeyValueResolver MongoDB persistence
+ * - A MongoDB persistence layer for the KeyValueResolver
+ * Copyright (C) 2022 Nitrobox GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nitrobox.keyvalueresolver.mongodbpersistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
